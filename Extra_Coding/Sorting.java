b993b6ba6cbf51f6e7f66ce7e5a10354516e1be7
SHEKH DALIM <156339532+Shekh-Dalim@users.noreply.github.com>
2025-10-02 16:34:58 +0530

    Update Sorting.java

--- Extra_Coding/Sorting.java
+++ Extra_Coding/Sorting.java
@@ -1,3 +1,53 @@
-public class Sorting {
-    
+/************************ Bubble Sort******************************/
+```
+import java.util.Scanner;
+
+public class ruhul {
+
+    public static void inputArry(int arr[], Scanner sc) {
+        System.out.println("Enter the array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    public static void displayArry(int arr[]) {
+        System.out.println("Display the array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] A, int n) {
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) { // Pass
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) { // Swap
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) { // no swapping -> already sorted
+                break;
+            }
+        }
+    }
+
+   
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        inputArry(arr, sc);
+        bubbleSort(arr, size);
+        displayArry(arr);
+
+    }
 }
+```
